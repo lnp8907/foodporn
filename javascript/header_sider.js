2fa34557b65492b2fdca83855a9f23f9b5776d13
foodpornDa106 <u0132094@gm.nkfust.edu.tw>
2020-03-19 00:49:32 +0800

    Update shop for first

--- javascript/header_sider.js
+++ javascript/header_sider.js
@@ -57,23 +57,6 @@ $('.slider').each(function () {
             }
         }, 4000);
     }
-
-    $('.next_btn').on('click', function () {
-        if (currentIndex < ($slides.length - 1)) {
-            move(currentIndex + 1);
-        } else {
-            move(0);
-        }
-    });
-
-    $('.previous_btn').on('click', function () {
-        if (currentIndex !== 0) {
-            move(currentIndex - 1);
-        } else {
-            move(3);
-        }
-    });
-
     $.each($slides, function (index) {
         var $button = $('<a class="slide_btn">&bull;</a>');
 
@@ -83,6 +66,82 @@ $('.slider').each(function () {
         $button.on('click', function () {
             move(index);
         }).appendTo('.slide_buttons');
+        bulletArray.push($button);
+    });
+
+    advance();
+});
+
+/*========================= shop slider==========================*/
+
+$('.shop-slider').each(function () {
+    var $this = $(this);
+    var $group = $this.find('.shop-slide_group');
+    var $slides = $this.find('.shop-slide');
+    var bulletArray = [];
+    var currentIndex = 0;
+    var timeout;
+
+    function move(newIndex) {
+        var animateLeft, slideLeft;
+
+        advance();
+
+        if ($group.is(':animated') || currentIndex === newIndex) {
+            return;
+        }
+
+        bulletArray[currentIndex].removeClass('active');
+        bulletArray[newIndex].addClass('active');
+
+        if (newIndex > currentIndex) {
+            slideLeft = '100%';
+            animateLeft = '-100%';
+        } else {
+            slideLeft = '-100%';
+            animateLeft = '100%';
+        }
+
+        $slides.eq(newIndex).css({
+            display: 'block',
+            left: slideLeft
+        });
+        $group.animate({
+            left: animateLeft
+        }, function () {
+            $slides.eq(currentIndex).css({
+                display: 'none'
+            });
+            $slides.eq(newIndex).css({
+                left: 0
+            });
+            $group.css({
+                left: 0
+            });
+            currentIndex = newIndex;
+        });
+    }
+
+    function advance() {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            if (currentIndex < ($slides.length - 1)) {
+                move(currentIndex + 1);
+            } else {
+                move(0);
+            }
+        }, 5000);
+    }
+
+    $.each($slides, function (index) {
+        var $button = $('<a class="shop-slide_btn">&bull;</a>');
+
+        if (index === currentIndex) {
+            $button.addClass('active');
+        }
+        $button.on('click', function () {
+            move(index);
+        }).appendTo('.shop-slide_buttons');
         bulletArray.push($button);
     });
 
